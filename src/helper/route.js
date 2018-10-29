93406a20ecd835b0edf90c6893606184880494b5
GeoDaoyu <geodaoyu@foxmail.com>
2018-10-29 15:20:53 +0800

    space

--- src/helper/route.js
+++ src/helper/route.js
@@ -1,5 +1,5 @@
 const fs= require('fs')
-const promisify= require('util').promisify
+const promisify = require('util').promisify
 const stat = promisify(fs.stat)
 const readdir = promisify(fs.readdir)
 
